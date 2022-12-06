cab85309d00d4d17a28658c8197fa7ca0fc10904
Irfan Ahmad <irfanibnuahmad@gmail.com>
2022-12-07 01:27:40 +0800

    added try accept in /filter endpoint

--- api/views.py
+++ api/views.py
@@ -31,39 +31,42 @@ def get_top_posts(post_id=None):
 
 @views.route("/filter", methods=["GET"])
 def filter_comments():
-    parser = RequestParser()
-    # comment
-    parser.add_argument("postId", type=int)
-    parser.add_argument("id", type=int)
-    parser.add_argument("name", type=str)
-    parser.add_argument("email", type=str)
-    parser.add_argument("body", type=str)
-
-    json_out = parser.parse_args()
-
     try:
-        assert isinstance(json_out["postId"], int) or json_out["postId"] is None, \
-            "post ID must be of type int"
-        assert isinstance(json_out["id"], int) or json_out["id"] is None, \
-            "ID must be of type int"
-        assert isinstance(json_out["name"], str) or json_out["name"] is None, \
-            "name must be of type str"
-        assert isinstance(json_out["email"], str) or json_out["email"] is None, \
-            "email must be of type str"
-        assert isinstance(json_out["body"], str) or json_out["body"] is None, \
-            "body must be of type str"
-    except AssertionError as e:
+        parser = RequestParser()
+        # comment
+        parser.add_argument("postId", type=int)
+        parser.add_argument("id", type=int)
+        parser.add_argument("name", type=str)
+        parser.add_argument("email", type=str)
+        parser.add_argument("body", type=str)
+
+        json_out = parser.parse_args()
+
+        try:
+            assert isinstance(json_out["postId"], int) or json_out["postId"] is None, \
+                "post ID must be of type int"
+            assert isinstance(json_out["id"], int) or json_out["id"] is None, \
+                "ID must be of type int"
+            assert isinstance(json_out["name"], str) or json_out["name"] is None, \
+                "name must be of type str"
+            assert isinstance(json_out["email"], str) or json_out["email"] is None, \
+                "email must be of type str"
+            assert isinstance(json_out["body"], str) or json_out["body"] is None, \
+                "body must be of type str"
+        except AssertionError as e:
+            return jsonify({"res": False, "msg": str(e)}), 400
+
+        # remove None values
+        json_out = {i: json_out[i] for i in json_out if json_out[i]}
+
+        comments = requests.get(COMMENTS).json()
+
+        for key in json_out:
+            if key in ["name", "email", "body"]:  # perform string matching
+                comments = [c for c in comments if json_out[key].lower() in c[key].lower()]
+            else:  # perform exact matching
+                comments = [c for c in comments if json_out[key] == c[key]]
+
+        return comments, 200
+    except Exception as e:
         return jsonify({"res": False, "msg": str(e)}), 400
-
-    # remove None values
-    json_out = {i: json_out[i] for i in json_out if json_out[i]}
-
-    comments = requests.get(COMMENTS).json()
-
-    for key in json_out:
-        if key in ["name", "email", "body"]:  # perform string matching
-            comments = [c for c in comments if json_out[key].lower() in c[key].lower()]
-        else:  # perform exact matching
-            comments = [c for c in comments if json_out[key] == c[key]]
-
-    return comments, 200
